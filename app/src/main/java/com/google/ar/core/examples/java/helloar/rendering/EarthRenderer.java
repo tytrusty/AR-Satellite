3b21aebc8c8992bf8df2e211f5657e1116662a2b
Ty Trusty <tytrusty@utexas.edu>
2017-12-31 17:28:31 -0600

    Refractor earth renderer

--- app/src/main/java/com/google/ar/core/examples/java/helloar/rendering/EarthRenderer.java
+++ app/src/main/java/com/google/ar/core/examples/java/helloar/rendering/EarthRenderer.java
@@ -39,19 +39,28 @@ import java.util.ArrayList;
 public class EarthRenderer extends ObjectRenderer {
     private static final String TAG = EarthRenderer.class.getSimpleName();
 
-    public EarthRenderer() {
-    }
+    public EarthRenderer() {}
 
-    private ArrayList<Float> vertices = new ArrayList<>();
-    private ArrayList<Float> texCoords = new ArrayList<>();
-    private ArrayList<Float> normals = new ArrayList<>();
-    private ArrayList<Integer> indices = new ArrayList<>();
     private final int stacks = 40;
     private final int slices = 40;
+
+    private final int verticesSize  = (stacks + 1) * (slices + 1) * 3;
+    private final int texCoordsSize = (stacks + 1) * (slices + 1) * 3;
+    private final int indicesSize   = (slices * stacks + slices) * 6;
+
+    private float[] vertices = new float[verticesSize];
+    private float[] normals = new float[verticesSize];
+    private float[] texCoords = new float[texCoordsSize];
+    private short[] indices = new short[indicesSize];
 
     public void create_sphere() {
         // Credit mostly to:
         // https://stackoverflow.com/questions/26116923/modern-opengl-draw-a-sphere-and-cylinder
+        int vertex_idx = 0;
+        int normal_idx = 0;
+        int tex_idx = 0;
+        int indices_idx = 0;
+
         for (int i = 0; i <= stacks; ++i){
 
             // PHI is the angle along the longitudinal lines
@@ -72,30 +81,31 @@ public class EarthRenderer extends ObjectRenderer {
                 float z = (float) (Math.sin (theta) * Math.sin  (phi));
                 float x = (float) (Math.cos (theta) * Math.sin  (phi));
 
-                // Push Back Vertex Data
-                vertices.add(x);
-                vertices.add(y);
-                vertices.add(z);
-                normals.add(x);
-                normals.add(y);
-                normals.add(z);
-                texCoords.add(1.0f - theta/ ((float)Math.PI * 2.0f));
-                texCoords.add(1.0f - phi/(float)Math.PI); // FLIP Orientation with 1 - ...
+                // Add vertex point
+                vertices[vertex_idx++] = x;
+                vertices[vertex_idx++] = y;
+                vertices[vertex_idx++] = z;
+
+                // Add normal point
+                normals[normal_idx++] = x;
+                normals[normal_idx++] = y;
+                normals[normal_idx++] = z;
+
+                texCoords[tex_idx++] = (1.0f - theta/ ((float)Math.PI * 2.0f));
+                texCoords[tex_idx++] = (1.0f - phi/(float)Math.PI); // FLIP Orientation with 1 - ...
             }
         }
 
         // Calc The Index Positions
-        for (int i = 0; i < slices * stacks + slices; ++i){
+        for (short i = 0; i < slices * stacks + slices; ++i){
+            indices[indices_idx++] = (i);
+            indices[indices_idx++] = (short) (i + slices + 1);
+            indices[indices_idx++] = (short) (i + slices);
 
-            indices.add (i);
-            indices.add (i + slices + 1);
-            indices.add (i + slices);
-
-            indices.add (i + slices + 1);
-            indices.add (i);
-            indices.add (i + 1);
+            indices[indices_idx++] = (short) (i + slices + 1);
+            indices[indices_idx++] = (i);
+            indices[indices_idx++] = (short) (i + 1);
         }
-        System.out.println("Numba of vertices loul: " + indices.size());
     }
     /**
      * Creates and initializes OpenGL resources needed for rendering the model.
@@ -132,29 +142,17 @@ public class EarthRenderer extends ObjectRenderer {
         mIndexBufferId = buffers[1];
 
         // Load vertex buffer
-        // What a mess, but it works lol
         create_sphere();
-        float[] vertArr = new float[vertices.size()];
-        float[] textArr = new float[texCoords.size()];
-        float[] normArr = new float[normals.size()];
-        short[] indicesArr = new short[indices.size()];
-
-        int i = 0; for (Float f : vertices) { vertArr[i++] = (f != null ? f : Float.NaN);}
-        i = 0; for (Float f : texCoords) { textArr[i++] = (f != null ? f : Float.NaN);}
-        i = 0; for (Float f : normals) { normArr[i++] = (f != null ? f : Float.NaN);}
-        i = 0;
-        for(i = 0; i < indices.size(); ++i) { indicesArr[i] = indices.get(i).shortValue();
-        }
 
 
-        FloatBuffer vertBuffer = FloatBuffer.allocate(vertices.size());
-        FloatBuffer texBuffer  = FloatBuffer.allocate(texCoords.size());
-        FloatBuffer normBuffer = FloatBuffer.allocate(normals.size());
-        vertBuffer.position(0); vertBuffer.put(vertArr); vertBuffer.rewind();
-        texBuffer.position(0); texBuffer.put(textArr); texBuffer.rewind();
-        normBuffer.position(0); normBuffer.put(normArr); normBuffer.rewind();
-        ShortBuffer indecesBuf = ShortBuffer.allocate(indices.size());
-        indecesBuf.position(0); indecesBuf.put(indicesArr); indecesBuf.rewind();
+        FloatBuffer vertBuffer = FloatBuffer.allocate(verticesSize);
+        FloatBuffer texBuffer  = FloatBuffer.allocate(texCoordsSize);
+        FloatBuffer normBuffer = FloatBuffer.allocate(verticesSize);
+        vertBuffer.position(0); vertBuffer.put(vertices); vertBuffer.rewind();
+        texBuffer.position(0); texBuffer.put(texCoords); texBuffer.rewind();
+        normBuffer.position(0); normBuffer.put(normals); normBuffer.rewind();
+        ShortBuffer indicesBuf = ShortBuffer.allocate(indicesSize);
+        indicesBuf.position(0); indicesBuf.put(indices); indicesBuf.rewind();
 
         mVerticesBaseAddress = 0;
         mTexCoordsBaseAddress = mVerticesBaseAddress + 4 * vertBuffer.limit();
@@ -164,18 +162,18 @@ public class EarthRenderer extends ObjectRenderer {
         GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, mVertexBufferId);
         GLES20.glBufferData(GLES20.GL_ARRAY_BUFFER, totalBytes, null, GLES20.GL_STATIC_DRAW);
         GLES20.glBufferSubData(
-                GLES20.GL_ARRAY_BUFFER, mVerticesBaseAddress, 4 * vertArr.length, vertBuffer);
+                GLES20.GL_ARRAY_BUFFER, mVerticesBaseAddress, 4 * vertices.length, vertBuffer);
         GLES20.glBufferSubData(
-                GLES20.GL_ARRAY_BUFFER, mTexCoordsBaseAddress, 4 * textArr.length, texBuffer);
+                GLES20.GL_ARRAY_BUFFER, mTexCoordsBaseAddress, 4 * texCoords.length, texBuffer);
         GLES20.glBufferSubData(
-                GLES20.GL_ARRAY_BUFFER, mNormalsBaseAddress, 4 * normArr.length, normBuffer);
+                GLES20.GL_ARRAY_BUFFER, mNormalsBaseAddress, 4 * normals.length, normBuffer);
         GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, 0);
 
         // Load index buffer
         GLES20.glBindBuffer(GLES20.GL_ELEMENT_ARRAY_BUFFER, mIndexBufferId);
-        mIndexCount = indecesBuf.limit();
+        mIndexCount = indicesBuf.limit();
         GLES20.glBufferData(
-                GLES20.GL_ELEMENT_ARRAY_BUFFER, 2 * mIndexCount, indecesBuf, GLES20.GL_STATIC_DRAW);
+                GLES20.GL_ELEMENT_ARRAY_BUFFER, 2 * mIndexCount, indicesBuf, GLES20.GL_STATIC_DRAW);
         GLES20.glBindBuffer(GLES20.GL_ELEMENT_ARRAY_BUFFER, 0);
 
         ShaderUtil.checkGLError(TAG, "OBJ buffer load");
